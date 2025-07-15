bc2c744ae13269e8d320d6ff57464e55579a1e53
Antony Perez <antonyperez0@yahoo.com>
2025-07-15 14:19:17 -0700

    deploy action

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // This line is essential for GitHub Pages deployment
+  base: "/portfolio-site/",
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.js',
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.js",
   },
-})
+});
