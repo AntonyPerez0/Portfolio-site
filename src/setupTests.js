de0278e6c55a8d2740f155d6cd834ccf13ff3df9
AntonyPerez0 <varadme@yahoo.com>
2026-06-11 16:21:42 -0700

    change

--- src/setupTests.js
+++
@@ -1,8 +0,0 @@
-import '@testing-library/jest-dom'
-
-beforeEach(() => {
-  const meta = document.createElement('meta');
-  meta.setAttribute('name', 'description');
-  meta.setAttribute('content', '');
-  document.head.appendChild(meta);
-});
