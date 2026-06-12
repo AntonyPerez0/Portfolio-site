e8703640643edf34fe4e8685bd533da81629c3b7
AntonyPerez0 <varadme@yahoo.com>
2026-06-11 18:38:19 -0700

    setpoints to points

--- src/main.jsx
+++ src/main.jsx
@@ -110,7 +110,7 @@ function Band({ maxSpeed = 50, minSpeed = 10 }) {
       curve.points[1].copy(j2.current.lerped)
       curve.points[2].copy(j1.current.lerped)
       curve.points[3].copy(fixed.current.translation())
-      band.current?.geometry?.setPoints(curve.getPoints(32))
+      band.current?.geometry?.points(curve.getPoints(32))
       ang.copy(card.current.angvel())
       rot.copy(card.current.rotation())
       card.current.setAngvel({ x: ang.x, y: ang.y - rot.y * 0.25, z: ang.z })
